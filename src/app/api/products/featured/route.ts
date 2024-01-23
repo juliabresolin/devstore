c0f483fa6ee43e3c054e911a9f88074ad639b937
@juliabresolin <julia.bresolin@jbq.global>
2024-01-23 13:35:05 -0300

    feat: home loading && fix tailwind config

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,7 @@
 import data from '../data.json'
 
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1000)) // delay 1s
   const featuredProducts = data.products.filter((product) => product.featured)
   return Response.json(featuredProducts)
 }
